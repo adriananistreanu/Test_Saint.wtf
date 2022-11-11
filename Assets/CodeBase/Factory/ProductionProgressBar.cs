3d2908078b36693a9d4d0105eee8f9462b418f97
adriananistreanu <67268831+adriananistreanu@users.noreply.github.com>
2022-11-11 12:28:01 +0200

    ui

---
+++ Assets/CodeBase/Factory/ProductionProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.Factory
+{
+    public class ProductionProgressBar : MonoBehaviour
+    {
+        [SerializeField] private Image fillBar;
+
+        private void Start()
+        {
+            ResetBar();
+        }
+
+        private void ResetBar()
+        {
+            fillBar.fillAmount = 1f;
+        }
+
+        public void UpdateBar(float duration)
+        {
+            ResetBar();
+            fillBar.DOFillAmount(0f, duration).SetEase(Ease.Linear);
+        }
+        
+        
+    }
+}
